83d9dac141173e0da4c8d16d1e1142faa5017431
Sachin Prajapati <skprajapati3214@gmail.com>
2024-11-12 19:08:49 +0530

    Add ImageRepository class for bitmap and byte array conversion

---
+++ app/src/main/java/com/skp3214/financepal/ImageRepository.kt
@@ -0,0 +1,51 @@
+package com.skp3214.financepal
+
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+
+import java.io.ByteArrayOutputStream
+
+class ImageRepository(private val resources: Resources) {
+
+    fun bitmapToByteArray(bitmap: Bitmap): ByteArray {
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, stream) // Use compression quality of 90
+        return stream.toByteArray()
+    }
+
+    fun byteArrayToBitmap(byteArray: ByteArray): Bitmap {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size, options)
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight)
+
+        options.inJustDecodeBounds = false
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size, options)
+    }
+
+    private val reqWidth: Int
+        get() = resources.getDimensionPixelSize(android.R.dimen.thumbnail_width)
+
+    private val reqHeight: Int
+        get() = resources.getDimensionPixelSize(android.R.dimen.thumbnail_height)
+
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        val height = options.outHeight
+        val width = options.outWidth
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+
+            val halfHeight = height / 2
+            val halfWidth = width / 2
+
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
+    }
+}
