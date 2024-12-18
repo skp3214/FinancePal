49815b3ee7acedcdbc8d58bfe8ece0b735ad2d20
Sachin Prajapati <skprajapati3214@gmail.com>
2024-12-18 20:34:57 +0530

    database initialization

---
+++ app/src/main/java/com/skp3214/financepal/DatabaseProvider.kt
@@ -0,0 +1,23 @@
+package com.skp3214.financepal
+
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseProvider {
+    private var INSTANCE: FinancePalDatabase? = null
+
+    fun getDatabase(context: Context): FinancePalDatabase {
+        if (INSTANCE == null) {
+            synchronized(FinancePalDatabase::class.java) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        FinancePalDatabase::class.java,
+                        "FinancePalDB"
+                    ).build()
+                }
+            }
+        }
+        return INSTANCE!!
+    }
+}
